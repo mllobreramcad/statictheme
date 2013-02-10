741874784744d2b038b13604411615d3d3797b43
Jessica Hearn <Jessica.Hearn.09@gmail.com>
2013-02-10 17:57:59 -0600

    Bloginfo calls implemented

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Static</title>
+	<title><?php bloginfo( 'name' ); ?></title>
 	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
 	<script src="http://use.edgefonts.net/merriweather.js"></script>
-	<link rel="stylesheet" href="wp-content/themes/statictheme/style.css"/>
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>"/>
 </head>
 	<body>
 		<header>
-			<h1>Static</h1>
+			<h1><?php bloginfo( 'name' ); ?></h1>
 			<nav>
 				<ul>
 					<li>Home</li>
@@ -19,6 +19,7 @@
 		<section>
 			<img src="wp-content/themes/statictheme/images/falcon.jpg" />
 			<section>
+				<h1>About Static</h1>
 				<p>Static is a figment of your imagination. It looks like a living, breathing website based on a CMS, but in reality it is only a static page that has been installed as a Wordpress theme. You will find addresses that lead to undetermined locations, links that have no destinations, images that are not where they appear to be. You're traveling through another dimension&mdash;a dimension not only of sight and sound but of mind. A journey into a wondrous land whose boundaries are that of imagination. That's a signpost up ahead: your next stop: the Twilight Zone!</p>
 				<p>Just kidding. Seriously, it's only a practice theme... ;)</p>
 				<p class="twist">(...Or is it? <em>Muahahahaha</em>........)</p>
