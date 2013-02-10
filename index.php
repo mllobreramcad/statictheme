e2ac884283b5c69fd582c7f6a25c871aa7de95f9
Jessica Hearn <Jessica.Hearn.09@gmail.com>
2013-02-10 16:59:18 -0600

    Initial commit of static theme files

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Static</title>
+	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
+	<script src="http://use.edgefonts.net/merriweather.js"></script>
+	<link rel="stylesheet" href="http://localhost/wordpresslab/wp-content/themes/statictheme/style.css"/>
+</head>
+	<body>
+		<header>
+			<h1>Static</h1>
+			<nav>
+				<ul>
+					<li>Home</li>
+					<li>WordPress</li>
+				</ul>
+			</nav>
+		</header>
+		<section>
+			<img src="http://localhost/wordpresslab/wp-content/themes/statictheme/images/falcon.jpg" />
+			<section>
+				<p>Static is a figment of your imagination. It looks like a living, breathing website based on a CMS, but in reality it is only a static page that has been installed as a Wordpress theme. You will find addresses that lead to undetermined locations, links that have no destinations, images that are not where they appear to be. You're traveling through another dimension&mdash;a dimension not only of sight and sound but of mind. A journey into a wondrous land whose boundaries are that of imagination. That's a signpost up ahead: your next stop: the Twilight Zone!</p>
+				<p>Just kidding. Seriously, it's only a practice theme... ;)</p>
+				<p class="twist">(...Or is it? <em>Muahahahaha</em>........)</p>
+			</section>
+		</section>
+		<footer>
+			<h1>Contact Us</h1>
+			<p>Static</p>
+			<p>12 Grimmauld Place, London</p>
+			<p>England, The World, The Universe</p>
+			<p>Phone: +44 20 1066 1701</p>
+		</footer>
+	</body>
+</html>
